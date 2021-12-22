f90012e5b026ce245236bad821aeda8207d742cf
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2021-12-22 14:02:45 -0300

    final

--- MovieAPI/src/main/java/com/example/newmovieapi/model/Pelicula.java
+++ MovieAPI/src/main/java/com/example/newmovieapi/model/Pelicula.java
@@ -1,7 +1,6 @@
 package com.example.newmovieapi.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
+@SuppressWarnings("serial")
 @Entity
 
 public class Pelicula implements Serializable {
